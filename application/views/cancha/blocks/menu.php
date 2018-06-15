b0ca1e745a30798f6019f3f6a9b5761bc0055757
Fabricio Orrala <fabricio@cajanegra.com.ec>
2018-06-14 22:18:54 -0500

    pantallas de apuestas

--- application/views/cancha/blocks/menu.php
+++ application/views/cancha/blocks/menu.php
@@ -72,7 +72,7 @@
       <a data-toggle="tooltip" data-placement="top" title="Refrescar" href="<?php echo site_url($this->uri->uri_string()); ?>">
         <span class="fa fa-refresh" aria-hidden="true"></span>
       </a>
-      <a data-toggle="tooltip" data-placement="top" title="Logs" href="<?php echo site_url('log/listar'); ?>">
+      <a data-toggle="tooltip" data-placement="top" title="Historial" href="#">
         <span class="fa fa-file-text" aria-hidden="true"></span>
       </a>
       <a data-toggle="tooltip" data-placement="top" title="Salir" href="<?php echo site_url('cancha/logout'); ?>">
@@ -111,82 +111,8 @@
                 <i class="fa fa-sign-out pull-right"></i> Salir
               </a>
             </li>
-            <li>
-              <a href="">
-                <i class="fa fa-sign-out pull-right"></i> Salir
-              </a>
-            </li>
           </ul>
         </li>
-
-        <?php /*
-
-        <li role="presentation" class="dropdown">
-          <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-            <i class="fa fa-envelope-o"></i>
-            <span class="badge bg-green">6</span>
-          </a>
-          <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-            <li>
-              <a>
-                <span class="image"><img src="<?php echo base_url('assets/apuestamundial/img/near-avatar.png'); ?>" alt="Profile Image" /></span>
-                <span>
-                  <span>John Smith</span>
-                  <span class="time">3 mins ago</span>
-                </span>
-                <span class="message">
-                  Film festivals used to be do-or-die moments for movie makers. They were where...
-                </span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span class="image"><img src="<?php echo base_url('assets/apuestamundial/img/near-avatar.png'); ?>" alt="Profile Image" /></span>
-                <span>
-                  <span>John Smith</span>
-                  <span class="time">3 mins ago</span>
-                </span>
-                <span class="message">
-                  Film festivals used to be do-or-die moments for movie makers. They were where...
-                </span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span class="image"><img src="<?php echo base_url('assets/apuestamundial/img/near-avatar.png'); ?>" alt="Profile Image" /></span>
-                <span>
-                  <span>John Smith</span>
-                  <span class="time">3 mins ago</span>
-                </span>
-                <span class="message">
-                  Film festivals used to be do-or-die moments for movie makers. They were where...
-                </span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span class="image"><img src="<?php echo base_url('assets/apuestamundial/img/near-avatar.png'); ?>" alt="Profile Image" /></span>
-                <span>
-                  <span>John Smith</span>
-                  <span class="time">3 mins ago</span>
-                </span>
-                <span class="message">
-                  Film festivals used to be do-or-die moments for movie makers. They were where...
-                </span>
-              </a>
-            </li>
-            <li>
-              <div class="text-center">
-                <a>
-                  <strong>See All Alerts</strong>
-                  <i class="fa fa-angle-right"></i>
-                </a>
-              </div>
-            </li>
-          </ul>
-        </li>
-        */ ?>
-
       </ul>
     </nav>
   </div>
--- application/views/cancha/templates/index.php
+++ application/views/cancha/templates/index.php
@@ -41,7 +41,7 @@
 				<div class="x_panel">
 					<div class="x_title">
 						<h2>
-							Últimos Resultados<small>Revisa tus apuestas</small>
+							Resultados oficiales<small>Últimos jugados</small>
 						</h2>
 						<div class="clearfix"></div>
 					</div>
@@ -73,7 +73,34 @@
 											<span class="flag-icon flag-icon-mx bandera-normal"></span>
 										</td>
 										<td class="text-center centrado-vertical">
-											<span class="label label-danger">No apostado</span>
+											<span class="label label-success">Ganada</span>
+										</td>
+									</tr>
+									<tr class="info-partido">
+										<td class="text-center centrado-vertical">
+											<div class="date">
+												<p class="month">Jun</p>
+												<p class="day">14</p>
+												<p class="time">10:48</p>
+											</div>
+										</td>
+										<td class="text-right centrado-vertical">
+											<span class="flag-icon flag-icon-ar bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Ecuador</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-amarillo texto-resultado">2:0</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Nueva Zelanda</span>
+										</td>
+										<td class="text-left centrado-vertical">
+											<span class="flag-icon flag-icon-ve bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="label label-danger">Perdida</span>
 										</td>
 									</tr>
 								</tbody>
@@ -83,17 +110,157 @@
 				</div>
 			</div>
 			<div class="col-md-6 col-sm-12 col-xs-12">
-				<div class="x_panel fondo-imagen fondo-2">
+				<div class="x_panel">
 					<div class="x_title">
 						<h2>
 							Próximos Partidos<small>Puedes apostar aún</small>
 						</h2>
 						<div class="clearfix"></div>
 					</div>
-					<div class="x_content">
-						<div class="row">
-							<div class="col-md-12">
-							</div>
+					<div class="x_content fondo-imagen fondo-2">
+						<div class="contenedor-tabla fondo-transparencia-negro-1">
+							<table class="table table-hover table-condensed tabla-partidos">
+								<tbody>
+									<tr class="info-partido">
+										<td class="text-center centrado-vertical">
+											<div class="date">
+												<p class="month">Jun</p>
+												<p class="day">14</p>
+												<p class="time">10:48</p>
+											</div>
+										</td>
+										<td class="text-right centrado-vertical">
+											<span class="flag-icon flag-icon-ru bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Arabia Saudí</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-amarillo">vs</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Estados Unidos</span>
+										</td>
+										<td class="text-left centrado-vertical">
+											<span class="flag-icon flag-icon-us bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<div class="btn btn-negro btn-amarillo-hover">
+												Apostar
+											</div>
+										</td>
+									</tr>
+									<tr class="info-partido">
+										<td class="text-center centrado-vertical">
+											<div class="date">
+												<p class="month">Jun</p>
+												<p class="day">14</p>
+												<p class="time">10:48</p>
+											</div>
+										</td>
+										<td class="text-right centrado-vertical">
+											<span class="flag-icon flag-icon-bo bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Bolivia</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-amarillo">vs</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco nombre-pais">Chile</span>
+										</td>
+										<td class="text-left centrado-vertical">
+											<span class="flag-icon flag-icon-cl bandera-normal"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<div class="btn btn-negro btn-amarillo-hover">
+												Apostar
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+  		</div>
+  		<div class="row">
+  			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>
+							Mesas abiertas<small>Apuesta contra ellos</small>
+						</h2>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content fondo-imagen fondo-3">
+						<div class="contenedor-tabla fondo-transparencia-negro-1 p-10">
+							<table class="table table-hover table-condensed tabla-partidos tabla-partidos-apuestas">
+								<thead>
+									<tr>
+										<th class="text-center centrado-vertical">
+											<span class="txt-amarillo text-uppercase">Fecha</span>
+										</th>
+										<th class="text-center centrado-vertical">
+											<span class="txt-amarillo text-uppercase">Partido</span>
+										</th>
+										<th class="text-center centrado-vertical">
+											<span class="txt-amarillo text-uppercase">Participante</span>
+										</th>
+										<th class="text-center centrado-vertical">
+											<span class="txt-amarillo text-uppercase">Monto</span>
+										</th>
+										<th class="text-center centrado-vertical" colspan="3">
+											<span class="txt-amarillo text-uppercase">Apuesta</span>
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="info-partido">
+										<td class="text-center centrado-vertical">
+											<div class="date">
+												<p class="month">Jun</p>
+												<p class="day">14</p>
+												<p class="time">10:48</p>
+											</div>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="flag-icon flag-icon-ec bandera-md"></span>
+											<span class="txt-blanco">Arabia Saudí</span>
+											<span class="txt-amarillo">vs</span>
+											<span class="txt-blanco">México</span>
+											<span class="flag-icon flag-icon-mx bandera-md"></span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco">
+												Fabricio Diógenes Orrala Parrales
+											</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<span class="txt-blanco">
+												$ 1.00
+											</span>
+										</td>
+										<td class="text-center centrado-vertical">
+											<div class="btn btn-negro btn-amarillo-hover">
+												Gana Local
+											</div>
+										</td>
+										<td class="text-center centrado-vertical">
+											<div class="btn btn-negro btn-amarillo-hover">
+												Empate
+											</div>
+										</td>
+										<td class="text-center centrado-vertical">
+											<div class="btn btn-negro btn-amarillo-hover">
+												Gana Visitante
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
